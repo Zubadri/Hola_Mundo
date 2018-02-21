d1cd0f568dc4e9332acefff10a2b5f405bf46e23
Zubadri <36697489+Zubadri@users.noreply.github.com>
2018-02-21 13:02:53 +0100

    Update PruebaGitHub.java

--- PruebaGitHub/src/pruebagithub/PruebaGitHub.java
+++ PruebaGitHub/src/pruebagithub/PruebaGitHub.java
@@ -17,6 +17,8 @@ public class PruebaGitHub {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Git");
+        System.out.println("Hello Gitters");
+        
     }
     
 }
